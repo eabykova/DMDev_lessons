c61a2693a9e81817a09ee9490417a9a843b8c6ae
circevet <circe_vet@mail.ru>
2022-09-23 11:30:43 +0300

    refactor Les20-21

--- src/Lesson20/LambdaExample.java
+++ src/Lesson20/LambdaExample.java
@@ -4,13 +4,21 @@ import java.util.Comparator;
 
 public class LambdaExample {
 	public static void main(String[] args) {
+		//Example of Lambda Expression since Java 1.8
 		Comparator<Integer> comparator = Integer::compare;
 		System.out.println(comparator.compare(34, 89));
-//	private static class IntegerComparator implements Comparator<Integer> {
-//
-//		@Override
-//		public int compare(Integer o1, Integer o2) {
-//			return Integer.compare(o1, o2);
-//		}
+
+		System.out.println();
+		//Example of simple method before Java 1.8
+		Comparator<Integer> comparator1 = new IntegerComparator();//with @FunctionalInterface only
+		System.out.println(comparator1.compare(34, 89));
+	}
+
+	private static class IntegerComparator implements Comparator<Integer> {
+
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			return Integer.compare(o1, o2);
+		}
 	}
 }
--- src/Lesson21/SteamExample.java
+++ src/Lesson21/SteamExample.java
@@ -1,6 +1,5 @@
 package Lesson21;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SteamExample {
