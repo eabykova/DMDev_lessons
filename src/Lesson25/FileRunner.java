dae7aa24d7cec1945a6f8222409b0ab7f4593bb3
circevet <circe_vet@mail.ru>
2022-09-28 16:10:08 +0300

    Les25

---
+++ src/Lesson25/FileRunner.java
@@ -0,0 +1,20 @@
+package Lesson25;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileRunner {
+	public static void main(String[] args) throws IOException {
+		File file = new File("D:\\PROGRAMMING\\PROJECTS\\DMDev_lessons\\src\\resources\\test.txt");
+		System.out.println("Before file creating");
+		System.out.println("Does the file/directory exists? : " + (file.exists() ? "yes" : "no"));
+		if (!file.exists()) {
+			System.out.println("Creating new file: " + file.createNewFile());
+			System.out.println("After file creating");
+			System.out.println("Does the file/directory exists? : " + (file.exists() ? "yes" : "no"));
+		}
+		System.out.println("Is it directory? : " + (file.isDirectory() ? "yes" : "no"));
+		System.out.println("Is it file? : " + (file.isFile() ? "yes" : "no"));
+		System.out.println("Name of the file is: " + file.getName() + "; parent is: " + file.getParent());
+	}
+}
---
+++ src/Lesson25/ReaderRunner.java
@@ -0,0 +1,18 @@
+package Lesson25;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+public class ReaderRunner {
+	public static void main(String[] args) throws IOException {
+		File file = Path.of("src", "resources", "test.txt").toFile();
+		try (BufferedReader fileReader = new BufferedReader(new FileReader(file))){
+			String collect = fileReader.lines()
+					.collect(Collectors.joining("\n"));
+			System.out.println(collect);
+		}
+
+
+	}
+}
