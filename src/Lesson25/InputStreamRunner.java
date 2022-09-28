4456d27394d5ccb690c32c14f8bde12c65a7990e
circevet <circe_vet@mail.ru>
2022-09-28 16:39:25 +0300

    Les25

--- src/Lesson25/InputStreamRunner.java
+++ src/Lesson25/InputStreamRunner.java
@@ -11,8 +11,10 @@ public class InputStreamRunner {
 //		File file = new File(String.join(File.separator,"src", "resources", "test.txt"));//before Java 1,7
 		File file = Path.of("src", "resources", "test.txt").toFile();
 		System.out.println(file.exists());
-		try (FileInputStream fileInputStream = new FileInputStream(file)) {
-			//reading byte by byte
+		try (FileInputStream fileInputStream = new FileInputStream(file)) {//during using of streams in try-with-resources
+																//the stream will be closed automatically after
+																//finish stream working
+			//reading byte by byte:
 			byte[] bytes = new byte[fileInputStream.available()];
 			int count = 0;
 			byte current;
---
+++ src/Lesson25/OutputRunner.java
@@ -0,0 +1,21 @@
+package Lesson25;
+
+import java.io.*;
+import java.nio.file.Path;
+
+public class OutputRunner {
+	public static void main(String[] args) throws IOException {
+		File file = Path.of("src", "resources", "output.txt").toFile();
+		try (FileOutputStream outputRunner = new FileOutputStream(file, true)) {//if we'll use "true" as the second param
+			String val = "New string\n";												//new string will be written after old string
+			outputRunner.write(val.getBytes());
+		}
+		//example with buffered stream:
+		File file1 = Path.of("src", "resources", "output1.txt").toFile();
+		try (BufferedOutputStream outputRunner1 = new BufferedOutputStream(new FileOutputStream(file1, true))) {
+			String val1 = "New string";
+			outputRunner1.write(val1.getBytes());
+			outputRunner1.write(System.lineSeparator().getBytes());//lineSeparator adds the newline symbol depending on OS
+		}
+	}
+}
