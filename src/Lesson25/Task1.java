9d2f2204ce79bad34b072ab3e5fb398d5a54d8f7
circevet <circe_vet@mail.ru>
2022-09-30 13:04:17 +0300

    les25 task1

---
+++ src/Lesson25/Task1.java
@@ -0,0 +1,22 @@
+package Lesson25;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.nio.file.Path;
+
+public class Task1 {
+	private static final String VOWELS = "aeiouy";
+	public static void main(String[] args) throws IOException {
+		Path path = Path.of("src", "resources", "test.txt");
+		try (Scanner scanner = new Scanner(path)) {
+			while(scanner.hasNext()) {
+				String word = scanner.next();
+				char firstChar = word.charAt(0);
+				if (VOWELS.indexOf(firstChar) != -1) {
+					System.out.println(word);
+				}
+			}
+		}
+	}
+}
