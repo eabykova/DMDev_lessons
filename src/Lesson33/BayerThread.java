a1a9e2399daaf56511533786e9f70d3def63199d
circevet <circe_vet@mail.ru>
2022-10-16 13:50:38 +0300

    revert

--- src/Lesson33/BayerThread.java
+++ src/Lesson33/BayerThread.java
@@ -12,17 +12,22 @@ public class BayerThread implements Runnable {
 	@Override
 	public void run() {
 		try {
-			while (true) {
-				if (!cashboxes.isEmpty()) {
-					Cashbox cashbox = cashboxes.remove();
-					System.out.println(Thread.currentThread().getName() + " serviced at the " + cashbox);
-					Thread.sleep(5L);
-					System.out.println(Thread.currentThread().getName() + " free " + cashbox);
-					cashboxes.add(cashbox);
-					break;
-				} else {
-					Thread.sleep(5L);
-					System.out.println(Thread.currentThread().getName() + " waiting for free cashbox");
+			synchronized (cashboxes) {
+				while (true) {
+					if (!cashboxes.isEmpty()) {
+						Cashbox cashbox = cashboxes.remove();
+						System.out.println(Thread.currentThread().getName() + " serviced at the " + cashbox);
+//						Thread.sleep(5L);
+						cashboxes.wait(5L);
+						System.out.println(Thread.currentThread().getName() + " free " + cashbox);
+						cashboxes.add(cashbox);
+						cashboxes.notifyAll();
+						break;
+					} else {
+//						Thread.sleep(5L);
+						cashboxes.wait();
+						System.out.println(Thread.currentThread().getName() + " waiting for free cashbox");
+					}
 				}
 			}
 
